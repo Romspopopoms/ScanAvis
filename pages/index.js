b1e59933652eca4e7795560884e1578d96c87314
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-16 14:36:18 +0100

    Description des modifications apportées

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,8 @@ import TarifsContent from '../components/page_tarifs';
 
 const Index = () => (
   <>
-    {/* Assurez-vous que la classe gradient-01 est définie dans votre fichier CSS global. */}
     <div className="gradient-01 fixed inset-0 z-0" />
-    <div className="relative z-10 min-h-screen"> {/* Utilisez min-h-screen pour s'assurer que le contenu couvre au moins toute la hauteur de l'écran. */}
+    <div className="relative z-10 min-h-screen">
       <Navbar />
       <Hero />
       <About />
