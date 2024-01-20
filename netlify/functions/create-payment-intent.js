ba657a1e641ba555a5ac7a60cc1e93cd7c927f5c
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-20 17:38:12 +0100

    Description des modifications apportées

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,6 +1,5 @@
 const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY || 'sk_test_51OPtGvDWmnYPaxs1DJZliUMMDttrNP1a4usU0uBgZgjnfe4Ho3WuCzFivSpwXhqL0YgVl9c41lbsuHI1O4nHAUhz00ibE6rzPX');
 
-// Assurez-vous que cette structure correspond à la structure des produits dans votre frontend.
 const productPrices = {
   base: 2000, // 20 euros en centimes
   bronze: 4000, // 40 euros en centimes
@@ -16,12 +15,10 @@ exports.handler = async (event) => {
   try {
     const { items } = JSON.parse(event.body);
 
-    // Vérifier si les items sont dans un format valide
     if (!Array.isArray(items)) {
       throw new Error('Invalid items format: Items should be an array');
     }
 
-    // Calculer le montant total à partir des items
     const totalAmount = items.reduce((total, item) => {
       const itemPrice = productPrices[item.id];
       if (itemPrice === undefined) {
@@ -30,23 +27,18 @@ exports.handler = async (event) => {
       return total + itemPrice * item.quantity;
     }, 0);
 
-    // Créer une intention de paiement avec Stripe
     const paymentIntent = await stripe.paymentIntents.create({
       amount: totalAmount,
       currency: 'eur',
     });
 
-    // Réponse en cas de succès
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
     };
   } catch (error) {
-    // Journaliser l'erreur pour le debugging
     console.error('Error:', error);
-
-    // Réponse en cas d'erreur
     return {
       statusCode: 500,
       headers: { 'Content-Type': 'application/json' },
