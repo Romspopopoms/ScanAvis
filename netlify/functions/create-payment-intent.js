e5fdd40ad853d13071bde121a43fc1338a601631
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-20 19:35:10 +0100

    Description des modifications apportées

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY || 'sk_test_51OPtGvDWmnYPaxs1DJZliUMMDttrNP1a4usU0uBgZgjnfe4Ho3WuCzFivSpwXhqL0YgVl9c41lbsuHI1O4nHAUhz00ibE6rzPX');
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY || 'sk_test_...');
 
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
@@ -9,29 +9,24 @@ exports.handler = async (event) => {
     const { items } = JSON.parse(event.body);
     console.log('Received items:', items);
 
-    // Vérifiez que 'items' est un tableau
     if (!Array.isArray(items)) {
       throw new Error('Invalid items format: Items should be an array');
     }
 
-    // Valider la structure des articles
     items.forEach((item) => {
-      if (typeof item.id !== 'string' || typeof item.quantity !== 'number') {
+      if ((!Number.isInteger(item.id) && typeof item.id !== 'string') || typeof item.quantity !== 'number') {
         throw new Error(`Invalid item structure: ${JSON.stringify(item)}`);
       }
     });
 
-    // Calculer le montant total
     const totalAmount = items.reduce((total, item) => {
-      // Assurez-vous que le prix est défini pour chaque article
-      const itemPrice = item.price; // Utilisez le prix fourni par le frontend
+      const itemPrice = item.price;
       if (typeof itemPrice === 'undefined') {
         throw new Error(`Price not found for item ID: ${item.id}`);
       }
       return total + itemPrice * item.quantity;
     }, 0);
 
-    // Créer l'intention de paiement
     const paymentIntent = await stripe.paymentIntents.create({
       amount: totalAmount,
       currency: 'eur',
