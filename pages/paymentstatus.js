682d1df626b871072401b671f520e79afb09d699
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-20 21:40:51 +0100

    Description des modifications apportées

--- pages/paymentstatus.js
+++ pages/paymentstatus.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const PaymentStatusPage = () => {
   const router = useRouter();
@@ -14,33 +16,37 @@ const PaymentStatusPage = () => {
   }
 
   return (
-    <div className="payment-status-page">
-      {paymentStatus === 'succeeded' ? (
-        <div className="success-message">
-          <h1>Merci pour votre achat !</h1>
-          <p>Votre transaction a été réalisée avec succès. Nous apprécions votre confiance et nous espérons que vous serez satisfait de votre achat.</p>
-          <Link href="/">
-            <button type="button" className="btn btn-primary">
-              Retour à l'accueil
-            </button>
-          </Link>
-        </div>
-      ) : (
-        <div className="error-message">
-          <h1>Oups, quelque chose s'est mal passé !</h1>
-          <p>Malheureusement, une erreur est survenue lors du traitement de votre paiement. Vous pouvez réessayer le paiement ou contacter notre support pour obtenir de l'aide.</p>
-          <Link href="/retry-payment">
-            <button type="button" className="btn btn-danger">
-              Réessayer le Paiement
-            </button>
-          </Link>
-          <Link href="/">
-            <button type="button" className="btn btn-secondary">
-              Retour à l'accueil
-            </button>
-          </Link>
-        </div>
-      )}
+    <div className="bg-primary-black overflow-hidden">
+      <Navbar />
+      <div className="payment-status-page">
+        {paymentStatus === 'succeeded' ? (
+          <div className="success-message">
+            <h1>Merci pour votre achat !</h1>
+            <p>Votre transaction a été réalisée avec succès. Nous apprécions votre confiance et nous espérons que vous serez satisfait de votre achat.</p>
+            <Link href="/">
+              <button type="button" className="btn btn-primary">
+                Retour à l'accueil
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <div className="error-message">
+            <h1>Oups, quelque chose s'est mal passé !</h1>
+            <p>Malheureusement, une erreur est survenue lors du traitement de votre paiement. Vous pouvez réessayer le paiement ou contacter notre support pour obtenir de l'aide.</p>
+            <Link href="/retry-payment">
+              <button type="button" className="btn btn-danger">
+                Réessayer le Paiement
+              </button>
+            </Link>
+            <Link href="/">
+              <button type="button" className="btn btn-secondary">
+                Retour à l'accueil
+              </button>
+            </Link>
+            <Footer />
+          </div>
+        )}
+      </div>
     </div>
   );
 };
