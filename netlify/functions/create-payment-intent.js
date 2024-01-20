f58fe3c47e1ea0760b7e148c345ea5433418be19
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-20 15:05:10 +0100

    Description des modifications apportées

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
 
 const productPrices = {
   base: 2000, // 20 euros en centimes
