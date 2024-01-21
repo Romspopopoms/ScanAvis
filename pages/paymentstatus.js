ce5b3e57500cf99b48cb94288f2b0d2ee25509af
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-21 02:52:48 +0100

    Description des modifications apportées

--- pages/paymentstatus.js
+++ pages/paymentstatus.js
@@ -6,46 +6,33 @@ import Footer from '../components/Footer';
 
 const PaymentStatusPage = () => {
   const router = useRouter();
-  const { paymentStatus, paymentIntentId } = router.query;
+  const { paymentStatus, paymentIntentId } = router.query; // Assurez-vous que paymentIntentId est passé en tant que paramètre de requête
   const [transaction, setTransaction] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (paymentStatus === 'succeeded' && paymentIntentId) {
-      setLoading(true);
-      fetch(
-        `/.netlify/functions/get-transaction?paymentIntentId=${paymentIntentId}`,
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.transaction) {
+    const fetchTransaction = async () => {
+      if (paymentStatus === 'succeeded' && paymentIntentId) {
+        try {
+          const response = await fetch(`/.netlify/functions/get-transaction?paymentIntentId=${paymentIntentId}`);
+          if (response.ok) {
+            const data = await response.json();
             setTransaction(data.transaction);
           } else {
-            setError('Transaction not found');
+            throw new Error('Failed to fetch transaction data');
           }
-          setLoading(false);
-        })
-        .catch((err) => {
-          console.error('Error fetching transaction:', err);
-          setError(err.message);
-          setLoading(false);
-        });
-    }
+        } catch (error) {
+          console.error('Error fetching transaction:', error);
+        }
+      }
+    };
+
+    fetchTransaction();
   }, [paymentStatus, paymentIntentId]);
-
-  if (loading) {
-    return <p>Chargement des détails de la transaction...</p>;
+  if (!transaction && paymentStatus === 'succeeded') {
+    return <p>Chargement des détails de la transaction...</p>; // Afficher un message de chargement
   }
 
-  if (error) {
-    return (
-      <p>
-        Erreur lors de la récupération des détails de la transaction: {error}
-      </p>
-    );
-  }
-
+  // Affichage en cas d'accès non autorisé ou de paramètre manquant
   if (!paymentStatus) {
     return (
       <div className="gradient-01 fixed inset-0 z-0">
@@ -64,24 +51,21 @@ const PaymentStatusPage = () => {
     <div className="relative z-10 min-h-screen">
       <Navbar />
       <div className="payment-status-page">
-        {paymentStatus === 'succeeded' && transaction ? (
+        {paymentStatus === 'succeeded' ? (
           <div className="success-message">
             <h1>Merci pour votre achat !</h1>
-            <p>
-              Votre transaction a été réalisée avec succès. Nous apprécions
-              votre confiance et nous espérons que vous serez satisfait de votre
-              achat.
-            </p>
-            <div className="order-summary">
-              <h2>Récapitulatif de la commande</h2>
-              <p>
-                Date de la commande:{' '}
-                {new Date(transaction.createdAt).toLocaleString()}
-              </p>
-              <p>ID de la transaction: {transaction.paymentIntentId}</p>
-              <p>Articles commandés: {JSON.stringify(transaction.items)}</p>
-              <p>Montant total: {transaction.totalAmount}€</p>
-            </div>
+            <p>Votre transaction a été réalisée avec succès. Nous apprécions votre confiance et nous espérons que vous serez satisfait de votre achat.</p>
+
+            {transaction && (
+              <div className="order-summary">
+                <h2>Récapitulatif de la commande</h2>
+                <p>Date de la commande: {new Date(transaction.createdAt).toLocaleString()}</p>
+                <p>ID de la transaction: {transaction.paymentIntentId}</p>
+                <p>Articles commandés: {JSON.stringify(transaction.items)}</p>
+                <p>Montant total: {transaction.totalAmount}€</p>
+              </div>
+            )}
+
             <Link href="/">
               <button type="button" className="btn btn-primary">
                 Retour à l'accueil
@@ -91,11 +75,7 @@ const PaymentStatusPage = () => {
         ) : (
           <div className="error-message">
             <h1>Oups, quelque chose s'est mal passé !</h1>
-            <p>
-              Malheureusement, une erreur est survenue lors du traitement de
-              votre paiement. Vous pouvez réessayer le paiement ou contacter
-              notre support pour obtenir de l'aide.
-            </p>
+            <p>Malheureusement, une erreur est survenue lors du traitement de votre paiement. Vous pouvez réessayer le paiement ou contacter notre support pour obtenir de l'aide.</p>
             <Link href="/retry-payment">
               <button type="button" className="btn btn-danger">
                 Réessayer le Paiement
