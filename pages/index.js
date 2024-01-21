89530335e015931913fb48cf0ca0612074b2d762
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-21 20:55:17 +0100

    Description des modifications apportées

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,17 @@ import { About, Explore, GetStarted, Hero, WhatsNew, World } from '../sections';
 import TarifsContent from '../components/page_tarifs';
 
 const Index = () => (
-  <>
-    <div className="gradient-01 fixed inset-0 z-0" />
-    <div className="relative z-10 min-h-screen">
-      <Navbar />
-      <Hero />
-      <About />
-      <TarifsContent />
-      <GetStarted />
-      <WhatsNew />
-      <Explore />
-      <World />
-      <Footer />
-    </div>
-  </>
+  <div className="relative z-10 min-h-screen">
+    <Navbar />
+    <Hero />
+    <About />
+    <TarifsContent />
+    <GetStarted />
+    <WhatsNew />
+    <Explore />
+    <World />
+    <Footer />
+  </div>
 );
 
 export default Index;
