7cfbd7bb0970f1062d1a7fccaca0e6c3fdecbb9b
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-21 01:03:25 +0100

    Description des modifications apportées

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,18 +1,21 @@
-// Importez Stripe et PlanetScale DB
-const stripe = require('stripe')('sk_test_51OPtGvDWmnYPaxs1DJZliUMMDttrNP1a4usU0uBgZgjnfe4Ho3WuCzFivSpwXhqL0YgVl9c41lbsuHI1O4nHAUhz00ibE6rzPX');
+// Import Stripe and PlanetScale DB
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { conn } = require('../../utils/db');
 
 exports.handler = async (event) => {
+  // Only allow POST method
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: JSON.stringify({ message: 'Method Not Allowed' }) };
   }
 
   try {
+    // Parse the body to get items
     const { items } = JSON.parse(event.body);
     if (!Array.isArray(items)) {
       throw new Error('Invalid items format: Items should be an array');
     }
 
+    // Calculate the total amount
     const totalAmount = items.reduce((total, item) => {
       if ((!Number.isInteger(item.id) && typeof item.id !== 'string') || typeof item.quantity !== 'number' || typeof item.price !== 'number') {
         throw new Error(`Invalid item structure: ${JSON.stringify(item)}`);
@@ -20,29 +23,36 @@ exports.handler = async (event) => {
       return total + item.price * item.quantity;
     }, 0);
 
+    // Create a payment intent with Stripe
     const paymentIntent = await stripe.paymentIntents.create({
       amount: totalAmount,
       currency: 'eur',
     });
     console.log(`PaymentIntent created: ${paymentIntent.id}`);
 
-    // Utilisez la connexion PlanetScale pour exécuter votre requête
+    // Execute the database insertion using PlanetScale
     const [rows] = await conn.execute(
       'INSERT INTO Transactions (items, totalAmount, paymentIntentId, clientSecret) VALUES (?, ?, ?, ?)',
       [JSON.stringify(items), totalAmount, paymentIntent.id, paymentIntent.client_secret],
     );
     console.log('Insertion into database successful', rows);
 
+    // Return success response
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ clientSecret: paymentIntent.client_secret }),
     };
   } catch (error) {
+    // Log the detailed error
     console.error('Error:', error);
+
+    // Log SQL specific errors if any
     if (error.sqlMessage) {
       console.error('SQL Error:', error.sqlMessage);
     }
+
+    // Return error response
     return {
       statusCode: 500,
       headers: { 'Content-Type': 'application/json' },
