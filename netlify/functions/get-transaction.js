0d09431c640addd8aa1b9206d51f49e97d03889c
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-22 02:25:52 +0100

    Description des modifications apportées

--- netlify/functions/get-transaction.js
+++ netlify/functions/get-transaction.js
@@ -11,9 +11,7 @@ exports.handler = async (event) => {
     };
   }
 
-  // Nettoyer le paymentIntentId pour enlever les espaces blancs et les caractères d'échappement
-  const paymentIntentId = event.queryStringParameters?.paymentIntentId.replace(/\s/g, '');
-
+  const paymentIntentId = event.queryStringParameters?.paymentIntentId?.replace(/\s/g, '');
   console.log('paymentIntentId nettoyé:', paymentIntentId);
 
   if (!paymentIntentId) {
@@ -26,9 +24,10 @@ exports.handler = async (event) => {
 
   try {
     const query = 'SELECT * FROM Transactions WHERE paymentIntentId = ?';
-    const [rows] = await conn.execute(query, [paymentIntentId]);
+    const results = await conn.execute(query, [paymentIntentId]);
+    const rows = results[0];
 
-    if (!rows.length) {
+    if (!rows || rows.length === 0) {
       console.log(`Transaction non trouvée pour paymentIntentId: ${paymentIntentId}`);
       return {
         statusCode: 404,
@@ -37,32 +36,19 @@ exports.handler = async (event) => {
       };
     }
 
-    // Assurez-vous que les données de l'article peuvent être analysées correctement
-    let items;
-    try {
-      items = JSON.parse(rows[0].items || '[]');
-    } catch (e) {
-      console.error('Erreur lors de l\'analyse des articles:', e);
-      return {
-        statusCode: 500,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ error: 'Erreur lors de l\'analyse des données de l\'article' }),
-      };
-    }
-
-    const transaction = {
-      transactionId: rows[0].transactionId,
-      items,
-      totalAmount: rows[0].totalAmount,
-      paymentIntentId: rows[0].paymentIntentId,
-      createdAt: rows[0].createdAt,
-      // Assurez-vous de ne pas renvoyer le clientSecret
-    };
+    const transaction = rows.map((row) => ({
+      transactionId: row.transactionId,
+      items: JSON.parse(row.items || '[]'),
+      totalAmount: row.totalAmount,
+      paymentIntentId: row.paymentIntentId,
+      createdAt: row.createdAt,
+      // Assurez-vous de ne pas renvoyer le clientSecret ici.
+    }));
 
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ transaction }),
+      body: JSON.stringify(transaction),
     };
   } catch (error) {
     console.error('Erreur de la base de données:', error);
