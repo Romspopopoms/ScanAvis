ed3f281ebd7dedb3d6fd2ce0e936f08e63c3c5ae
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-28 02:02:13 +0100

    Description des modifications apportées

--- components/mon-profil.jsx
+++ components/mon-profil.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../context/AuthContext';
 
 const MonProfil = () => {
-  const { user, logout } = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const [userPayments, setUserPayments] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
@@ -55,7 +55,6 @@ const MonProfil = () => {
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Mon Profil</h1>
         <p className="text-red-500">{error}</p>
-        <button type="button" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition" onClick={logout}>Déconnexion</button>
       </div>
     );
   }
@@ -63,7 +62,6 @@ const MonProfil = () => {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Mon Profil</h1>
-      <button type="button" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition" onClick={logout}>Déconnexion</button>
 
       <div className="mt-8">
         <div className="bg-white shadow-md rounded-lg p-6 mb-6">
