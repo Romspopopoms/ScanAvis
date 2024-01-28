d24ecc356745613c395b38e59bd5ee4a48a7bacf
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-01-28 22:54:19 +0100

    Description des modifications apportées

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,10 @@ import Head from 'next/head';
 import React from 'react';
 import { useRouter } from 'next/router';
 import { CartProvider } from '../context/CartContext';
-import { AuthProvider } from '../context/AuthContext'; // Importez AuthProvider
+import { AuthProvider } from '../context/AuthContext';
 import CartSummary from '../components/CartSummary';
+import Navbar from '../components/Navbar'; // Assurez-vous que le chemin d'accès est correct
+import Footer from '../components/Footer'; // Assurez-vous que le chemin d'accès est correct
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
@@ -15,15 +17,24 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <AuthProvider> {/* Enrober avec AuthProvider */}
       <CartProvider>
-        <Head>
-          <title>Scan'Avis</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://stijndv.com" />
-          <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-        </Head>
-        {shouldDisplayCartSummary && <CartSummary />}
-        <Component {...pageProps} />
+        <div className="flex flex-col min-h-screen gradient-01">
+          <Head>
+            <title>Scan'Avis</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="preconnect" href="https://stijndv.com" />
+            <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+          </Head>
+
+          <Navbar />
+
+          <main className="flex-grow">
+            {shouldDisplayCartSummary && <CartSummary />}
+            <Component {...pageProps} />
+          </main>
+
+          <Footer />
+        </div>
       </CartProvider>
     </AuthProvider>
   );
--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
-import { Navbar, Footer } from '../components';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('');
@@ -13,7 +12,7 @@ const LoginPage = () => {
     setErrorMessage('');
 
     if (!username || !password) {
-      setErrorMessage('Le nom d\'utilisateur et le mot de passe sont requis');
+      setErrorMessage("Le nom d'utilisateur et le mot de passe sont requis");
       return;
     }
 
@@ -41,54 +40,96 @@ const LoginPage = () => {
       if (authUrl) {
         window.location.href = authUrl;
       } else {
-        setErrorMessage('Erreur lors de la récupération de l\'URL d\'authentification');
+        setErrorMessage(
+          "Erreur lors de la récupération de l'URL d'authentification",
+        );
       }
     } catch (error) {
-      setErrorMessage('Erreur lors de la récupération de l\'URL d\'authentification');
+      setErrorMessage(
+        "Erreur lors de la récupération de l'URL d'authentification",
+      );
     }
   };
 
   return (
-    <div className="flex flex-col min-h-screen page-container gradient-01">
-      <Navbar />
-      <div className="flex-grow">
-        <div className="flex items-center justify-center px-4 sm:px-6 lg:px-8">
-          <div className="w-full max-w-md">
-            <form className="space-y-6 rounded-lg bg-white p-8 shadow-lg" onSubmit={handleSubmit}>
-              <h1 className="mb-6 text-center text-5xl font-extrabold text-gray-900">Connexion</h1>
-              {errorMessage && <p className="mb-4 text-center text-sm text-red-600">{errorMessage}</p>}
-              <div>
-                <label htmlFor="username" className="block text-sm font-medium text-gray-700">Nom d'utilisateur</label>
-                <input id="username" type="text" required className="mt-1 mb-4 w-full rounded-md border py-2 px-4 text-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500" placeholder="Entrez votre nom d'utilisateur" autoComplete="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </div>
-
-              <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700">Mot de passe</label>
-                <input id="password" type="password" required className="mt-1 mb-6 w-full rounded-md border py-2 px-4 text-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500" placeholder="Entrez votre mot de passe" autoComplete="current-password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-              <button type="submit" className="mb-4 w-full rounded-md bg-blue-500 py-3 px-4 text-white shadow-md transition duration-200 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">Se connecter</button>
-
-              <div className="mt-6 text-center text-sm text-gray-700">
-                <a href="/" className="font-medium text-blue-600 hover:underline">Mot de passe oublié ?</a>
-              </div>
-              <hr className="my-6" />
-              <button type="button" className="w-full rounded-md bg-red-600 py-3 px-4 text-white shadow-md transition duration-200 ease-in-out hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
-                onClick={handleGoogleSignIn}
-              >
-                Connexion avec Google
-              </button>
-              <p className="mt-6 text-center text-sm text-gray-700">Pas encore de compte ? <a href="/" className="font-medium text-blue-600 hover:underline">Inscrivez-vous</a></p>
-            </form>
+    <div className="flex items-center justify-center px-4 sm:px-6 lg:px-8 min-h-screen">
+      <div className="w-full max-w-md">
+        <form
+          className="space-y-6 rounded-lg bg-white p-8 shadow-lg"
+          onSubmit={handleSubmit}
+        >
+          <h1 className="mb-6 text-center text-5xl font-extrabold text-gray-900">
+            Connexion
+          </h1>
+          {errorMessage && (
+            <p className="mb-4 text-center text-sm text-red-600">
+              {errorMessage}
+            </p>
+          )}
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Nom d'utilisateur
+            </label>
+            <input
+              id="username"
+              type="text"
+              required
+              className="mt-1 mb-4 w-full rounded-md border py-2 px-4 text-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              placeholder="Entrez votre nom d'utilisateur"
+              autoComplete="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
-          <div className="footer-gradient w-full absolute bottom-0 left-0 right-0" />
-          <Footer />
-        </div>
+
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Mot de passe
+            </label>
+            <input
+              id="password"
+              type="password"
+              required
+              className="mt-1 mb-6 w-full rounded-md border py-2 px-4 text-gray-700 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              placeholder="Entrez votre mot de passe"
+              autoComplete="current-password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button
+            type="submit"
+            className="mb-4 w-full rounded-md bg-blue-500 py-3 px-4 text-white shadow-md transition duration-200 ease-in-out hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+          >
+            Se connecter
+          </button>
+
+          <div className="mt-6 text-center text-sm text-gray-700">
+            <a href="/" className="font-medium text-blue-600 hover:underline">
+              Mot de passe oublié ?
+            </a>
+          </div>
+          <hr className="my-6" />
+          <button
+            type="button"
+            className="w-full rounded-md bg-red-600 py-3 px-4 text-white shadow-md transition duration-200 ease-in-out hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+            onClick={handleGoogleSignIn}
+          >
+            Connexion avec Google
+          </button>
+          <p className="mt-6 text-center text-sm text-gray-700">
+            Pas encore de compte ?{' '}
+            <a href="/" className="font-medium text-blue-600 hover:underline">
+              Inscrivez-vous
+            </a>
+          </p>
+        </form>
       </div>
     </div>
   );
