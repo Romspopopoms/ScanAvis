83725042b188ba24ef0be263360ef8293e0ab629
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-01 03:50:15 +0100

    Description des modifications apportées

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,42 @@
+// _app.js
 import '../styles/globals.css';
 import Head from 'next/head';
 import React from 'react';
 import { useRouter } from 'next/router';
 import { CartProvider } from '../context/CartContext';
 import { AuthProvider } from '../context/AuthContext';
+import { PaymentProvider } from '../context/PaymentContext'; // Importez PaymentProvider
 import CartSummary from '../components/CartSummary';
-import Navbar from '../components/Navbar'; // Assurez-vous que le chemin d'accès est correct
-import Footer from '../components/Footer'; // Assurez-vous que le chemin d'accès est correct
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
-
-  // Décider si le résumé du panier doit être affiché
   const shouldDisplayCartSummary = router.pathname !== '/paiement';
 
   return (
-    <AuthProvider> {/* Enrober avec AuthProvider */}
+    <AuthProvider>
       <CartProvider>
-        <div className="flex flex-col min-h-screen gradient-01">
-          <Head>
-            <title>Scan'Avis</title>
-            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-            <link rel="icon" href="/favicon.ico" />
-            <link rel="preconnect" href="https://stijndv.com" />
-            <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
-          </Head>
+        <PaymentProvider> {/* Enrober avec PaymentProvider */}
+          <div className="flex flex-col min-h-screen gradient-01">
+            <Head>
+              <title>Scan'Avis</title>
+              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              <link rel="icon" href="/favicon.ico" />
+              <link rel="preconnect" href="https://stijndv.com" />
+              <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
+            </Head>
 
-          <Navbar />
+            <Navbar />
 
-          <main className="flex-grow">
-            {shouldDisplayCartSummary && <CartSummary />}
-            <Component {...pageProps} />
-          </main>
+            <main className="flex-grow">
+              {shouldDisplayCartSummary && <CartSummary />}
+              <Component {...pageProps} />
+            </main>
 
-          <Footer />
-        </div>
+            <Footer />
+          </div>
+        </PaymentProvider>
       </CartProvider>
     </AuthProvider>
   );
