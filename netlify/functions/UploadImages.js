378b50f5b129a276a716461317118766f1846cf6
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-03 20:31:02 +0100

    Description des modifications apportées

--- netlify/functions/UploadImages.js
+++ netlify/functions/UploadImages.js
@@ -1,8 +1,28 @@
 const Busboy = require('busboy');
-const simpleGit = require('simple-git'); // Correction ici
+const { Octokit } = require('@octokit/rest');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
+
+// Définir uploadFile avant son utilisation
+async function uploadFile(file, octokit) {
+  try {
+    const contentBuffer = fs.readFileSync(file.filepath);
+    const contentEncoded = contentBuffer.toString('base64');
+
+    await octokit.repos.createOrUpdateFileContents({
+      owner: 'Romspopopoms',
+      repo: 'ScanAvis',
+      path: `uploaded_images/${file.filename}`, // Adjust the path according to your directory structure
+      message: `Add new image via serverless function: ${file.filename}`,
+      content: contentEncoded,
+    });
+    console.log(`File created: ${file.filename}`);
+  } catch (error) {
+    console.error('File upload failed:', error);
+    throw new Error(`File upload failed: ${file.filename}`);
+  }
+}
 
 exports.handler = async (event) => {
   console.log('Handler started');
@@ -10,6 +30,10 @@ exports.handler = async (event) => {
     console.log('Invalid HTTP method');
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
+
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_ACCESS,
+  });
 
   return new Promise((outerResolve, outerReject) => {
     const busboy = new Busboy({ headers: event.headers });
@@ -43,41 +67,17 @@ exports.handler = async (event) => {
       console.log('Done parsing form!');
       const writtenFiles = await Promise.all(fileWrites);
 
-      try {
-        const git = simpleGit();
-        console.log('Git config - setting user name and email');
-        await git.addConfig('user.name', 'Romspopopoms');
-        await git.addConfig('user.email', 'roman.2009.fr');
+      const fileUploadPromises = writtenFiles.map((file) => uploadFile(file, octokit));
 
-        console.log('Git add - adding files to staging');
-        await git.add('.'); // Ajoute tous les fichiers modifiés
-
-        console.log('Git commit - committing changes');
-        await git.commit('Votre message de commit');
-
-        // Ajoutez et commitez chaque fichier image
-        const commitPromises = writtenFiles.map(async (file) => {
-          console.log('Adding and committing image:', file.filepath);
-          await git.add(file.filepath);
-          await git.commit(`Add new image via serverless function: ${file.filename}`);
+      Promise.all(fileUploadPromises)
+        .then(() => {
+          console.log('All files uploaded successfully');
+          outerResolve({ statusCode: 200, body: 'All files uploaded successfully' });
+        })
+        .catch((error) => {
+          console.error('Operation failed:', error);
+          outerReject(new Error('Operation failed'));
         });
-
-        await Promise.all(commitPromises);
-
-        console.log('Git push - pushing to repository');
-        await git.push(['-u', 'origin', 'main', '--force'], {
-          GIT_SSH_COMMAND: 'ssh -o StrictHostKeyChecking=no',
-          GITHUB_TOKEN: process.env.GITHUB_ACCESS,
-        });
-        console.log('Image uploaded successfully');
-        outerResolve({ statusCode: 200, body: 'Image uploaded successfully' });
-      } catch (gitError) {
-        console.error('Git operation failed:', gitError);
-        const error = new Error('Git operation failed');
-        error.statusCode = 500;
-        error.body = 'Git operation failed';
-        outerReject(error);
-      }
     });
 
     if (event.isBase64Encoded) {
