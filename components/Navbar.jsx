a9d65ab8587e0a6e4b72ce1da935aae484032c72
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-03 05:31:19 +0100

    Description des modifications apportées

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,83 +7,97 @@ import { AuthContext } from '../context/AuthContext';
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const [navbarHeight, setNavbarHeight] = useState(0); // Définir la variable d'état pour la hauteur du navbar
-  const navbarRef = useRef(); // Ref pour accéder au DOM du navbar
   const { isAuthenticated, logout } = useContext(AuthContext);
+  const navbarRef = useRef(null);
+  const [navbarHeight, setNavbarHeight] = useState(0);
 
-  // Mise en place d'un effet pour mettre à jour la hauteur du navbar
   useEffect(() => {
-    const updateNavbarHeight = () => {
+    if (navbarRef.current) {
       setNavbarHeight(navbarRef.current.offsetHeight);
+    }
+
+    const handleResize = () => {
+      if (navbarRef.current) {
+        setNavbarHeight(navbarRef.current.offsetHeight);
+      }
     };
 
-    // Mettre à jour la hauteur lors du premier rendu
-    updateNavbarHeight();
+    window.addEventListener('resize', handleResize);
 
-    // Ajouter un écouteur d'événement pour les changements de taille de la fenêtre
-    window.addEventListener('resize', updateNavbarHeight);
-
-    // Nettoyer l'écouteur d'événement lors du démontage
     return () => {
-      window.removeEventListener('resize', updateNavbarHeight);
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
   const handleToggleMenu = () => setIsMenuOpen(!isMenuOpen);
   const toggleCart = () => setIsCartOpen(!isCartOpen);
 
-  const menuVariants = {
-    open: { x: 0 },
-    closed: { x: '-100%' },
+  const sidebarVariants = {
+    open: { x: 0, opacity: 1 },
+    closed: { x: '100%', opacity: 0 },
   };
 
   return (
     <>
-      <nav ref={navbarRef} className="fixed top-0 left-0 w-full flex justify-between items-center px-4 bg-gray-900 text-white z-50">
+      <motion.nav
+        ref={navbarRef}
+        className="fixed top-0 left-0 w-full flex justify-between items-center px-4 bg-purple-900 text-gray-100 z-50"
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ type: 'spring', stiffness: 260, damping: 20 }}
+      >
         <h2 className="font-extrabold text-2xl">
           <Link href="/">SCAN'AVIS</Link>
         </h2>
         <div className="flex items-center">
           {isAuthenticated ? (
-            <button
-              type="button"
+            <button type="button"
               onClick={logout}
               className="px-4 py-2 bg-red-600 hover:bg-red-700 transition rounded text-white"
-              aria-label="Déconnexion"
             >
               Déconnexion
             </button>
           ) : (
-            <Link href="/login" className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition rounded text-white">
+            <Link href="/login"
+              className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition rounded text-white"
+            >
               Se connecter
+
             </Link>
           )}
-          <div onClick={toggleCart} className="relative cursor-pointer ml-4">
-            <img src="/cart-icon.svg" alt="Cart" className="w-6 h-6" />
+          <div onClick={toggleCart} className="ml-4 relative cursor-pointer">
+            <img src="/cart-icon.svg" alt="Panier" className="w-6 h-6" />
           </div>
-          <button
-            type="button"
-            onClick={handleToggleMenu}
-            className="ml-4"
-            aria-label="Menu"
-          >
-            <img src="/menu.svg" alt="Menu" className="w-6 h-6" />
+          <button type="button" onClick={handleToggleMenu} className="ml-4">
+            <img src="/menu-icon.svg" alt="Menu" className="w-6 h-6" />
           </button>
         </div>
-      </nav>
+      </motion.nav>
 
       <motion.div
         initial="closed"
         animate={isMenuOpen ? 'open' : 'closed'}
-        variants={menuVariants}
-        className="fixed left-0 w-64 bg-gray-800 text-white shadow-xl z-40 p-5"
-        style={{ top: `${navbarHeight}px` }} // Appliquer la hauteur du navbar pour le style top du sidebar
+        variants={sidebarVariants}
+        className="fixed right-0 top-0 h-full bg-purple-800 text-white shadow-xl z-40 p-5"
+        style={{ top: navbarHeight }}
         transition={{ type: 'spring', stiffness: 260, damping: 20 }}
       >
-        <div className="mt-2"> {/* Ajouter une marge en haut pour éviter le chevauchement avec le navbar */}
-          {isAuthenticated && <Link href="/mon-profil" className="block hover:text-gray-300">Mon Profil</Link>}
-          <Link href="/" className="block hover:text-gray-300">Accueil</Link>
-          <Link href="/tarifs" className="block hover:text-gray-300">Nos offres</Link>
+        <div className="mt-2">
+          {isAuthenticated && (
+            <Link href="/mon-profil"
+              className="block hover:text-gray-300"
+            >Mon Profil
+            </Link>
+          )}
+          <Link href="/"
+            className="block hover:text-gray-300"
+          >Accueil
+          </Link>
+          <Link href="/tarifs"
+            className="block hover:text-gray-300"
+          >Nos offres
+          </Link>
+          {/* Autres liens du menu */}
         </div>
       </motion.div>
 
