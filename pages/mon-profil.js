de5f729872cca1fc91ef4ca4c3d389699587cdca
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-03 04:09:51 +0100

    Description des modifications apportées

--- pages/mon-profil.js
+++ pages/mon-profil.js
@@ -30,24 +30,26 @@ const MonProfilPage = () => {
 
   return (
     <div className="bg-gradient-to-b from-purple-800 to-purple-500">
-      {/* Retirer "fixed" et "top-0" pour permettre au menu de se déplacer avec le scroll */}
-      <div className="flex justify-center space-x-4 p-4 bg-purple-800 shadow-md">
-        {menuItems.map((item) => (
-          <motion.div
-            key={item.key}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.95 }}
-            className={`cursor-pointer px-4 py-2 rounded-lg text-white shadow-lg 
-                        ${activeSection === item.key ? 'bg-purple-600' : 'bg-purple-700 hover:bg-purple-600'}`}
-            onClick={() => setActiveSection(item.key)}
-          >
-            {item.name}
-          </motion.div>
-        ))}
+      {/* Navbar fixed en haut de la page */}
+      <div className="fixed top-0 w-full z-10">
+        <div className="flex justify-center space-x-4 p-4 bg-purple-800 shadow-md">
+          {menuItems.map((item) => (
+            <motion.div
+              key={item.key}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              className={`cursor-pointer px-4 py-2 rounded-lg text-white shadow-lg 
+                          ${activeSection === item.key ? 'bg-purple-600' : 'bg-purple-700 hover:bg-purple-600'}`}
+              onClick={() => setActiveSection(item.key)}
+            >
+              {item.name}
+            </motion.div>
+          ))}
+        </div>
       </div>
 
-      {/* Ajouter du padding pour pousser le contenu vers le bas */}
-      <div className="pt-16">
+      {/* Ajouter du padding en haut égal à la hauteur de la navbar pour éviter que le contenu soit caché derrière */}
+      <div className="pt-[4rem]"> {/* Ajustez cette valeur en fonction de la hauteur de votre navbar */}
         <AnimatePresence>
           {menuItems.map((item) => activeSection === item.key && (
             <motion.div
