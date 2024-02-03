cdd6175acecdfe95dd4d5497626c177f59db79ea
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-03 06:15:01 +0100

    Description des modifications apportées

--- pages/mon-profil.js
+++ pages/mon-profil.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import MonProfil from '../components/mon-profil';
 
@@ -9,21 +9,12 @@ const menuItems = [
 ];
 
 const variants = {
-  open: { opacity: 1, x: 0 },
-  collapsed: { opacity: 0, x: '-100%' },
+  open: { opacity: 1, height: 'auto' },
+  collapsed: { opacity: 0, height: 0 },
 };
 
 const MonProfilPage = () => {
   const [activeSection, setActiveSection] = useState('profil');
-  const [navbarHeight, setNavbarHeight] = useState(0);
-
-  useEffect(() => {
-    // Assurez-vous que l'ID ou la classe ciblée correspond à votre barre de navigation
-    const navbar = document.getElementById('Navbar'); // Utilisez l'ID ou la classe de votre barre de navigation
-    if (navbar) {
-      setNavbarHeight(navbar.offsetHeight);
-    }
-  }, []);
 
   const renderSection = (key) => {
     switch (key) {
@@ -38,21 +29,23 @@ const MonProfilPage = () => {
   };
 
   return (
-    <div className="bg-gradient-to-b from-purple-800 to-purple-500 min-h-screen" style={{ paddingTop: `${navbarHeight}px` }}>
-      {/* Ici, nous avons supprimé la div shadow-md car vous vouliez enlever le fond */}
-      <div className="flex justify-center space-x-4 p-4">
-        {menuItems.map((item) => (
-          <motion.div
-            key={item.key}
-            whileHover={{ scale: 1.1 }}
-            whileTap={{ scale: 0.95 }}
-            className={`cursor-pointer px-4 py-2 rounded-lg text-white 
-                ${activeSection === item.key ? 'bg-purple-600' : 'bg-purple-700 hover:bg-purple-600'}`}
-            onClick={() => setActiveSection(item.key)}
-          >
-            {item.name}
-          </motion.div>
-        ))}
+    <div className="bg-gradient-to-b from-purple-800 to-purple-500 min-h-screen">
+      {/* Appliquez une marge supérieure fixe au lieu de compter sur la hauteur calculée de la navbar */}
+      <div className="pt-20 lg:pt-24">
+        <div className="flex justify-center space-x-4 p-4">
+          {menuItems.map((item) => (
+            <motion.div
+              key={item.key}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              className={`cursor-pointer px-4 py-2 rounded-lg text-white 
+                            ${activeSection === item.key ? 'bg-purple-600' : 'bg-purple-700 hover:bg-purple-600'}`}
+              onClick={() => setActiveSection(item.key)}
+            >
+              {item.name}
+            </motion.div>
+          ))}
+        </div>
       </div>
       <AnimatePresence>
         {menuItems.map((item) => activeSection === item.key && (
