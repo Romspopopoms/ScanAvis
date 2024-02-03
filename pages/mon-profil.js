2667da643fb2239fe1cdbab12028e39d5919c14c
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-03 03:44:09 +0100

    Description des modifications apportées

--- pages/mon-profil.js
+++ pages/mon-profil.js
@@ -30,28 +30,25 @@ const MonProfilPage = () => {
 
   return (
     <div className="relative z-10 bg-gradient-to-b from-purple-800 to-purple-500">
-      {/* Ajout de padding-top pour tenir compte de la hauteur du menu */}
-      <div className="pt-16">
-        {/* Menu fixe en haut de la page */}
-        <div className="fixed top-0 left-0 w-full z-30">
-          <div className="flex justify-center space-x-4 p-4 bg-purple-800 shadow-md">
-            {menuItems.map((item) => (
-              <motion.div
-                key={item.key}
-                whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.95 }}
-                className={`cursor-pointer px-4 py-2 rounded-lg text-white shadow-lg 
-                            ${activeSection === item.key ? 'bg-purple-600' : 'bg-purple-700 hover:bg-purple-600'}`}
-                onClick={() => setActiveSection(item.key)}
-              >
-                {item.name}
-              </motion.div>
-            ))}
-          </div>
+      {/* Ajout de padding-top pour créer de l'espace pour le menu */}
+      <div className="pt-4"> {/* Ajustez cette valeur comme nécessaire pour l'espace du menu */}
+        <div className="flex justify-center space-x-4 p-4 bg-purple-800 shadow-md">
+          {menuItems.map((item) => (
+            <motion.div
+              key={item.key}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              className={`cursor-pointer px-4 py-2 rounded-lg text-white shadow-lg 
+                          ${activeSection === item.key ? 'bg-purple-600' : 'bg-purple-700 hover:bg-purple-600'}`}
+              onClick={() => setActiveSection(item.key)}
+            >
+              {item.name}
+            </motion.div>
+          ))}
         </div>
 
-        {/* Ajout de padding-top ici pour pousser le contenu en dessous du menu */}
-        <div className="pt-24"> {/* Ajustez cette valeur si nécessaire */}
+        {/* Ajustement du padding-top pour pousser le contenu en dessous du menu */}
+        <div className="pt-16"> {/* Cette valeur pourrait nécessiter un ajustement en fonction de la hauteur de votre menu */}
           <AnimatePresence>
             {menuItems.map((item) => activeSection === item.key && (
               <motion.div
