9bdffcb983b6c6f0e53160e07f8dff40801df836
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-05 00:32:53 +0100

    Description des modifications apportées

--- netlify/functions/ReturnHtml.js
+++ netlify/functions/ReturnHtml.js
@@ -20,30 +20,22 @@ async function generateHtmlPage(pageId) {
     const reactContent = `
       import React from 'react';
       
-      <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: 'url("${imageDeFondURL}")' }}>
-    {/* Container pour centrer le contenu verticalement et horizontalement */}
-    <div className="flex flex-col items-center justify-center h-full">
-      {/* Titre en tête de la page */}
-      <div className="absolute top-0 mt-10">
-        <h1 className="text-2xl text-white font-semibold">${titre}</h1>
-      </div>
-      {/* Contenu principal au centre */}
-      <div className="text-center">
-        <img src=${logoURL} alt="Logo" className="mb-6 w-20 h-auto" />
-        <div className="bg-white bg-opacity-75 rounded-lg p-4 shadow-lg mb-6">
-          <input
-            type="email"
-            placeholder="Entrez votre email"
-            className="appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
+      const Page = () => (
+        <div className= "relative bg-cover bg-center h-screen" style={{ backgroundImage: 'url("${imageDeFondURL}")' }}>
+          <div className="flex flex-col items-center justify-center h-full">
+          <div className="absolute top-0 mt-10">
+            <img src="${logoURL}" alt="Logo" className="mb-4" />
+            <h1 className="text-4xl text-white mb-4">${titre}</h1>
+            <div className="bg-white opacity-75 rounded p-4">
+              <input
+                type="email"
+                placeholder="Entrez votre email"
+                className="appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+          </div>
         </div>
-        <button type="button" className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-full transition ease-in-out duration-300">
-          Envoyer
-        </button>
-      </div>
-    </div>
-  </div>
-);
+      );
       
       export default Page;
     `;
