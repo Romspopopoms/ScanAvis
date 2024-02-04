f3ec25b4a59466172b9151a623813a44a023ac0c
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-05 00:43:04 +0100

    Description des modifications apportées

--- netlify/functions/ReturnHtml.js
+++ netlify/functions/ReturnHtml.js
@@ -18,29 +18,29 @@ async function generateHtmlPage(pageId) {
 
     // Générer le contenu HTML (ou React JSX)
     const reactContent = `
-      import React from 'react';
-      
-      const Page = () => (
-        <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("${imageDeFondURL}")' }}>
-          <div className="flex flex-col items-center justify-center h-full">
-            <img src="${logoURL}" alt="Logo" className="mb-6 w-20 h-auto" />
-            <h1 className="text-4xl text-white mb-4">${titre}</h1>
-            <div className="bg-white bg-opacity-75 rounded-lg p-4 shadow-lg mb-6">
-              <input
-                type="email"
-                placeholder="Entrez votre email"
-                className="appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              />
-            </div>
-            <button type="button" className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-full transition ease-in-out duration-300">
-          Envoyer
-        </button>
+    import React from 'react';
+    
+    const Page = () => (
+      <div className="bg-cover bg-center h-screen" style={{ backgroundImage: 'url("${imageDeFondURL}")' }}>
+        <div className="flex flex-col items-center justify-center h-full">
+          <img src="${logoURL}" alt="Logo" className="mb-6 w-20 h-auto" />
+          <h1 className="text-4xl text-white mb-4">${titre}</h1>
+          <div className="bg-white bg-opacity-75 rounded-lg p-4 shadow-lg mb-6">
+            <input
+              type="email"
+              placeholder="Entrez votre email"
+              className="appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
           </div>
+          <button type="button" className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-6 rounded-full transition ease-in-out duration-300">
+            Envoyer
+          </button>
         </div>
-      );
+      </div>
+    );
 
-      export default Page;
-    `;
+    export default Page;
+  `;
 
     // Utiliser Prettier pour formater le code généré
     const formattedReactContent = prettier.format(reactContent, { parser: 'babel', singleQuote: true, // force l'utilisation de guillemets simples
