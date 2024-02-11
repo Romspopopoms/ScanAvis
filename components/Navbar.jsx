ccd43d424cbd3060dbf4bffd1e00f75629c03bc0
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-11 09:45:26 +0100

    Description des modifications apportées

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,20 +12,15 @@ const Navbar = () => {
   const [navbarHeight, setNavbarHeight] = useState(0);
 
   useEffect(() => {
-    if (navbarRef.current) {
-      setNavbarHeight(navbarRef.current.offsetHeight);
-    }
-
-    const handleResize = () => {
-      if (navbarRef.current) {
-        setNavbarHeight(navbarRef.current.offsetHeight);
-      }
+    const updateNavbarHeight = () => {
+      setNavbarHeight(navbarRef.current?.offsetHeight || 0);
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener('resize', updateNavbarHeight);
+    updateNavbarHeight(); // Initial call to set the navbar height
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', updateNavbarHeight);
     };
   }, []);
 
@@ -33,28 +28,27 @@ const Navbar = () => {
   const toggleCart = () => setIsCartOpen(!isCartOpen);
 
   const sidebarVariants = {
-    open: { x: 0, opacity: 1 },
-    closed: { x: '-100%', opacity: 0 },
+    open: { x: 0 },
+    closed: { x: '100%' },
   };
 
   return (
     <>
       <motion.nav
         ref={navbarRef}
-        className="fixed top-0 left-0 w-full flex justify-between items-center px-4 py-2 bg-white text-gray-800 z-50 shadow-md"
+        className="fixed top-0 left-0 w-full flex justify-between items-center px-6 py-3 bg-opacity-90 backdrop-filter backdrop-blur-lg bg-white/30 text-gray-800 z-50 shadow-lg"
         initial={{ y: -100 }}
         animate={{ y: 0 }}
         transition={{ type: 'spring', stiffness: 260, damping: 20 }}
       >
-        <h2 className="text-xl font-bold">
-          <Link href="/">SCAN'AVIS</Link>
-        </h2>
-        <div className="flex items-center">
-          <div onClick={toggleCart} className="ml-4 cursor-pointer">
-            <img src="/cart-icon.svg" alt="Panier" className="w-6 h-6" />
-          </div>
-          <button type="button" onClick={handleToggleMenu} className="ml-4">
-            <img src="/menu-icon.svg" alt="Menu" className="w-6 h-6" />
+        <Link href="/"
+          className="text-xl md:text-2xl font-bold tracking-tighter"
+        >
+          SCAN'AVIS
+        </Link>
+        <div>
+          <button type="button" onClick={handleToggleMenu} className="text-gray-800 outline-none">
+            <img src="/menu.png" alt="Menu" className="w-8 h-8" />
           </button>
         </div>
       </motion.nav>
@@ -63,36 +57,42 @@ const Navbar = () => {
         initial="closed"
         animate={isMenuOpen ? 'open' : 'closed'}
         variants={sidebarVariants}
-        className="fixed top-0 left-0 h-full z-40 w-3/4 bg-white shadow-xl p-5"
-        style={{ top: `${navbarHeight}px` }}
-        transition={{ type: 'spring', stiffness: 260, damping: 20 }}
+        className="fixed inset-y-0 right-0 z-40 w-5/6 sm:w-2/3 md:w-1/2 lg:w-1/3 xl:w-1/4 bg-white shadow-xl p-6 flex flex-col justify-between transform transition-transform duration-300 ease-in-out"
+        style={{ top: navbarHeight }}
+        transition={{ type: 'tween' }}
       >
-        <div className="flex flex-col space-y-4">
-          {isAuthenticated ? (
-            <>
-              <Link href="/mon-profil" className="text-gray-800 hover:text-gray-600">
-                Mon Profil
-              </Link>
-              <button type="button"
-                onClick={logout}
-                className="text-gray-800 hover:text-gray-600"
-              >
-                Déconnexion
-              </button>
-            </>
-          ) : (
-            <Link href="/login" className="text-gray-800 hover:text-gray-600">
-              Se connecter
-            </Link>
-          )}
-          <Link href="/" className="text-gray-800 hover:text-gray-600">
+        <button type="button" onClick={handleToggleMenu} className="self-end">
+          <img src="/iconarrow.png" alt="Fermer" className="w-8 h-8" />
+        </button>
+        <nav className="mt-8 flex-grow">
+          <Link href="/mon-profil"
+            className="block py-2 text-gray-800 hover:text-gray-600 transition duration-150"
+          >
+            Mon Profil
+          </Link>
+          <Link href="/"
+            className="block py-2 text-gray-800 hover:text-gray-600 transition duration-150"
+          >
             Accueil
           </Link>
-          <Link href="/tarifs" className="text-gray-800 hover:text-gray-600">
+          <Link href="/tarifs"
+            className="block py-2 text-gray-800 hover:text-gray-600 transition duration-150"
+          >
             Nos offres
           </Link>
-          {/* Ajoute d'autres liens ici au besoin */}
-        </div>
+          {/* Autres liens */}
+        </nav>
+        {isAuthenticated && (
+          <button type="button"
+            onClick={() => {
+              logout();
+              handleToggleMenu();
+            }}
+            className="w-full py-3 mt-4 bg-red-600 hover:bg-red-700 text-white transition-colors duration-300 rounded-lg"
+          >
+            Déconnexion
+          </button>
+        )}
       </motion.div>
 
       {isCartOpen && <CartSummary isCartOpen={isCartOpen} toggleCart={toggleCart} />}
