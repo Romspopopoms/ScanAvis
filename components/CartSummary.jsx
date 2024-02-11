61d52431a014bde0244f4df027d020ea01b54ad7
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-11 11:47:45 +0100

    Description des modifications apportées

--- components/CartSummary.jsx
+++ components/CartSummary.jsx
@@ -3,8 +3,6 @@ import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { useCart } from '../context/CartContext';
 
-// Supposons que vous ayez un objet qui associe priceIds à leurs montants
-// Cela pourrait être placé dans un fichier séparé et importé si utilisé à plusieurs endroits
 const priceAmounts = {
   price_1OekFIDWmnYPaxs1hMaRWF4X: 2000, // Montant pour Base
   price_1OekKWDWmnYPaxs1W80kG5a0: 4000, // Montant pour Bronze
@@ -15,58 +13,55 @@ const priceAmounts = {
 const CartSummary = ({ isCartOpen }) => {
   const { cartItem, removeFromCart, clearCart } = useCart();
 
-  // Animation variants for the cart summary panel
   const cartVariants = {
     open: { x: 0, opacity: 1, scale: 1, transition: { type: 'spring', stiffness: 120 } },
     closed: { x: '-100%', opacity: 0, scale: 0.95, transition: { type: 'spring', stiffness: 120 } },
   };
 
-  // Utilisez priceId pour obtenir le coût total à partir de l'objet priceAmounts
   const totalCost = cartItem ? priceAmounts[cartItem.priceId] : 0;
 
   return (
     <motion.div
-      className={`fixed bottom-0 left-0 z-50 p-4 bg-white rounded-tl-3xl rounded-tr-3xl shadow-xl transform transition-transform ${isCartOpen ? 'translate-x-0' : '-translate-x-full'
-      }`}
+      className={`fixed inset-x-0 bottom-0 z-50 p-6 bg-white rounded-t-3xl shadow-xl transform transition-transform ${isCartOpen ? 'translate-x-0' : '-translate-x-full'}`}
       initial="closed"
       animate={isCartOpen ? 'open' : 'closed'}
       variants={cartVariants}
     >
-      <div className="overflow-y-auto max-h-96">
-        <h2 className="text-lg font-bold text-gray-800 mb-4">Mon Panier</h2>
+      <div className="flex flex-col max-h-96 overflow-y-auto">
+        <h2 className="text-xl font-bold text-gray-800 mb-4">Mon Panier</h2>
         {cartItem ? (
           <div className="flex justify-between items-center mb-4">
             <div>
               <h4 className="font-semibold">{cartItem.name}</h4>
               <p className="text-sm text-gray-600">${(totalCost / 100).toFixed(2)}</p>
             </div>
-            <button
-              type="button"
-              className="text-red-500 text-xs"
-              onClick={() => removeFromCart()}
+            <button type="button"
+              onClick={() => removeFromCart(cartItem.id)}
+              className="text-red-500 hover:text-red-600 transition-colors duration-300"
             >
-              Retirer
+              &#10005;
             </button>
           </div>
         ) : (
           <p className="text-gray-600">Votre panier est vide.</p>
         )}
-        <div className="mt-4">
-          <strong className="text-lg">Total: ${(totalCost / 100).toFixed(2)}</strong>
-          <div className="mt-4 flex justify-between">
-            <button
-              type="button"
-              className="bg-red-500 text-white text-sm px-3 py-1 rounded hover:bg-red-600 transition-colors duration-300"
+        <div className="mt-auto">
+          <div className="flex justify-between items-center border-t pt-4">
+            <strong className="text-lg">Total: ${(totalCost / 100).toFixed(2)}</strong>
+            <button type="button"
               onClick={clearCart}
+              className="text-sm bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-colors duration-300"
             >
               Vider le Panier
             </button>
-            <Link href="/paiement"
-              className="inline-block bg-green-500 text-white text-sm px-3 py-1 rounded hover:bg-green-600 transition-colors duration-300 ml-2"
-            >
-              S’abonner
-            </Link>
           </div>
+          {cartItem && (
+            <Link href="/paiement"
+              className="mt-4 block text-center text-sm bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition-colors duration-300"
+            >
+              Passer au paiement
+            </Link>
+          )}
         </div>
       </div>
     </motion.div>
--- context/AuthContext.js
+++ context/AuthContext.js
@@ -7,145 +7,75 @@ export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
   const [userSubscriptions, setUserSubscriptions] = useState([]);
-  const [subscriptionsUpdate, setSubscriptionsUpdate] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const [entreprise, setEntreprise] = useState('');
   const [googleBusiness, setGoogleBusiness] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
 
   const clearError = () => setErrorMessage('');
   const handleError = (message) => setErrorMessage(message);
 
-  const logout = () => {
+  const logout = useCallback(() => {
     localStorage.removeItem('user');
     localStorage.removeItem('authToken');
     setIsAuthenticated(false);
     setUser(null);
     setUserSubscriptions([]);
-  };
+  }, []);
 
-  const triggerRerender = () => {
-    // Mettre à jour le compteur pour forcer le re-rendu
-    setSubscriptionsUpdate(subscriptionsUpdate + 1);
-  };
+  const fetchUserSubscriptions = useCallback(async (uuid) => {
+    if (isLoading) return;
 
-  const fetchUserSubscriptions = async (uuid) => {
-    if (isLoading) return; // Pour éviter les appels en boucle
-
-    setIsLoading(true); // Commencer le chargement
-
+    setIsLoading(true);
     try {
-      const response = await fetch(`/.netlify/functions/getUserSubscriptions?userUuid=${uuid}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      });
+      const response = await fetch(`/.netlify/functions/getUserSubscriptions?userUuid=${uuid}`);
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-      if (!response.ok) {
-        if (response.status === 404) {
-          setUserSubscriptions([]);
-        } else {
-          throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
-        }
-      } else {
-        const data = await response.json();
-        setUserSubscriptions(data.subscriptions || []);
-      }
+      const data = await response.json();
+      setUserSubscriptions(data.subscriptions || []);
     } catch (error) {
-      console.error('Erreur lors de la récupération des abonnements utilisateur:', error);
-      handleError(`Erreur lors de la récupération des abonnements: ${error.message}`);
+      handleError(`Error fetching user subscriptions: ${error.message}`);
     } finally {
-      setIsLoading(false); // Terminer le chargement
-      triggerRerender(); // Forcer le re-rendu après chaque fetch, réussi ou non
+      setIsLoading(false);
     }
-  };
+  }, [isLoading]);
 
   const fetchUserDetails = useCallback(async (uuid) => {
     try {
-      const response = await fetch(`/.netlify/functions/getUserDetails?userUuid=${uuid}`, {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-      });
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
-      }
+      const response = await fetch(`/.netlify/functions/getUserDetails?userUuid=${uuid}`);
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
       const data = await response.json();
-      // Mise à jour du contexte avec les nouvelles informations
-      if (data.email) setUser((prevUser) => ({ ...prevUser, email: data.email }));
-      if (data.entreprise) setEntreprise(data.entreprise);
-      if (data.googleBusiness) setGoogleBusiness(data.googleBusiness);
+      setUser((prevUser) => ({ ...prevUser, ...data }));
+      setEntreprise(data.entreprise);
+      setGoogleBusiness(data.googleBusiness);
     } catch (error) {
-      console.error('Erreur lors de la récupération des détails de l’utilisateur :', error);
-      handleError(`Erreur lors de la récupération des détails : ${error.message}`);
+      handleError(`Error fetching user details: ${error.message}`);
     }
   }, []);
-
-  const updateUserDetails = useCallback(async (newEmail, newEntreprise, newGoogleBusiness) => {
-    if (!user || !user.uuid) {
-      console.error('Aucun utilisateur connecté pour mettre à jour les détails.');
-      setErrorMessage('Vous devez être connecté pour mettre à jour les informations.');
-      return;
-    }
-
-    try {
-      const response = await fetch('/.netlify/functions/updateUserDetails', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userUuid: user.uuid,
-          email: newEmail,
-          entreprise: newEntreprise,
-          googleBusiness: newGoogleBusiness,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Erreur HTTP! statut: ${response.status}`);
-      }
-
-      setUser((prevUser) => ({ ...prevUser, email: newEmail }));
-      setEntreprise(newEntreprise);
-      setGoogleBusiness(newGoogleBusiness);
-      console.log('Informations de l\'utilisateur mises à jour avec succès.');
-    } catch (error) {
-      console.error('Erreur lors de la mise à jour des détails de l\'utilisateur:', error);
-      setErrorMessage(`Erreur lors de la mise à jour: ${error.message}`);
-    }
-  }, [user, setUser, setEntreprise, setGoogleBusiness]); // Assurez-vous d'inclure toutes les dépendances externes utilisées dans la fonction
-
-  useEffect(() => {
-    if (user?.uuid) {
-      fetchUserDetails(user.uuid);
-    }
-  }, [user?.uuid, fetchUserDetails]);
 
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
       const userData = JSON.parse(storedUser);
-      setUser(userData);
       setIsAuthenticated(true);
+      setUser(userData);
       fetchUserSubscriptions(userData.uuid);
       fetchUserDetails(userData.uuid);
     }
-  }, []);
+  }, [fetchUserSubscriptions, fetchUserDetails]);
 
   const handleResubscribe = async (subscriptionId) => {
     try {
       const response = await fetch('/.netlify/functions/resubscribeSubscription', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ subscriptionId, userUuid: user.uuid }),
+        body: JSON.stringify({ subscriptionId, userUuid: user?.uuid }),
       });
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-      if (!response.ok) throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
-
-      await fetchUserSubscriptions(user.uuid);
+      fetchUserSubscriptions(user?.uuid);
     } catch (error) {
-      console.error('Erreur lors de la réabonnement:', error);
-      handleError(`Erreur lors de la réabonnement: ${error.message}`);
+      handleError(`Error during resubscription: ${error.message}`);
     }
   };
 
@@ -153,123 +83,66 @@ export const AuthProvider = ({ children }) => {
     try {
       const response = await fetch('/.netlify/functions/cancelSubscription', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ subscriptionId, userUuid: user.uuid }),
+        body: JSON.stringify({ subscriptionId, userUuid: user?.uuid }),
       });
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
 
-      if (!response.ok) throw new Error(`Erreur HTTP ! Statut : ${response.status}`);
-
-      await fetchUserSubscriptions(user.uuid);
+      fetchUserSubscriptions(user?.uuid);
     } catch (error) {
-      console.error('Erreur lors de la désabonnement:', error);
-      handleError(`Erreur lors de la désabonnement: ${error.message}`);
+      handleError(`Error during unsubscription: ${error.message}`);
     }
   };
 
-  // Déclarée avant la première utilisation
-  const verifyTokenWithServer = async (token) => {
+  const verifyTokenWithServer = useCallback(async (token) => {
     try {
       const response = await fetch('/.netlify/functions/verifyToken', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ idToken: token }),
       });
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
       const data = await response.json();
-      if (response.ok && data.user) {
+      if (data.user) {
         setIsAuthenticated(true);
         setUser({
-          email: data.user.email,
-          name: data.user.name,
-          access_token: data.user.access_token,
-          uuid: data.user.uuid,
+          ...data.user,
         });
         localStorage.setItem('authToken', data.user.access_token);
         fetchUserSubscriptions(data.user.uuid);
       } else {
         logout();
-        handleError(data.error || 'Erreur lors de la vérification du token');
+        handleError(data.error || 'Token verification error');
       }
     } catch (error) {
       logout();
-      handleError(`Erreur lors de la vérification du token: ${error.message}`);
+      handleError(`Token verification error: ${error.message}`);
     }
-  };
+  }, [fetchUserSubscriptions, logout]);
 
-  const getAuthUrl = async () => {
-    clearError();
-    try {
-      const response = await fetch('/.netlify/functions/request', { method: 'GET' });
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const { url } = await response.json();
-      return url;
-    } catch (error) {
-      handleError(`Erreur lors de la récupération de l'URL d'authentification: ${error.message}`);
-    }
-  };
-
-  const handleAuthResponse = async (response) => {
-    clearError();
-    try {
-      const data = await response.json();
-      if (response.ok && data.user) {
-        const userData = { // Définir userData à partir de data.user
-          uuid: data.user.uuid,
-          email: data.user.email,
-          name: data.user.name,
-          access_token: data.user.access_token,
-        };
-        localStorage.setItem('user', JSON.stringify(userData)); // Sauvegarder dans le localStorage
-        setUser(userData); // Mettre à jour l'état de l'utilisateur
-        setIsAuthenticated(true); // Mettre à jour l'état d'authentification
-        fetchUserSubscriptions(data.user.uuid); // Charger les abonnements de l'utilisateur
-      } else {
-        handleError(data.error || 'Erreur lors du traitement de la réponse.');
-      }
-    } catch (error) {
-      handleError(`Erreur lors du traitement de la réponse: ${error.message}`);
-    }
-  };
-
-  const handleAuthCode = async (code) => {
+  const handleAuthCode = useCallback(async (code) => {
     clearError();
     try {
       const response = await fetch('/.netlify/functions/oauth', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ code }),
       });
       if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      await handleAuthResponse(response);
-    } catch (error) {
-      handleError(`Erreur lors de l'envoi du code: ${error.message}`);
-    }
-  };
 
-  const refreshAccessToken = async (refreshToken) => {
-    try {
-      const response = await fetch('/.netlify/functions/refresh-token', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ refreshToken }),
-      });
       const data = await response.json();
-      if (response.ok) {
-        const { accessToken } = data;
-        localStorage.setItem('authToken', accessToken);
-        setUser((currentUser) => ({ ...currentUser, access_token: accessToken }));
+      if (data.user) {
         setIsAuthenticated(true);
+        setUser({ ...data.user });
+        localStorage.setItem('user', JSON.stringify(data.user));
+        fetchUserSubscriptions(data.user.uuid);
       } else {
-        logout();
-        handleError(data.error || 'Erreur lors du rafraîchissement du token.');
+        handleError(data.error || 'Error processing the auth code');
       }
     } catch (error) {
-      logout();
-      handleError(`Erreur lors du rafraîchissement du token: ${error.message}`);
+      handleError(`Error sending auth code: ${error.message}`);
     }
-  };
+  }, [fetchUserSubscriptions]);
 
   useEffect(() => {
-    clearError();
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
     const token = localStorage.getItem('authToken');
@@ -278,36 +151,53 @@ export const AuthProvider = ({ children }) => {
     } else if (token) {
       verifyTokenWithServer(token);
     }
-  }, []);
+  }, [handleAuthCode, verifyTokenWithServer]);
+
+  const refreshAccessToken = async (refreshToken) => {
+    try {
+      const response = await fetch('/.netlify/functions/refresh-token', {
+        method: 'POST',
+        body: JSON.stringify({ refreshToken }),
+      });
+      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+      const data = await response.json();
+      if (data.accessToken) {
+        localStorage.setItem('authToken', data.accessToken);
+        setUser((currentUser) => ({ ...currentUser, access_token: data.accessToken }));
+        setIsAuthenticated(true);
+      } else {
+        logout();
+        handleError(data.error || 'Error refreshing token');
+      }
+    } catch (error) {
+      logout();
+      handleError(`Token refresh error: ${error.message}`);
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{
-      isAuthenticated,
-      user,
-      userSubscriptions,
-      handleResubscribe,
-      handleCancelSubscription,
-      getAuthUrl,
-      handleAuthCode,
-      logout,
-      verifyTokenWithServer,
-      refreshAccessToken,
-      errorMessage,
-      handleError,
-      setSubscriptionsUpdate,
-      subscriptionsUpdate,
-      fetchUserSubscriptions,
-      entreprise,
-      setEntreprise,
-      googleBusiness,
-      setGoogleBusiness,
-      fetchUserDetails,
-      updateUserDetails,
-    }}
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        user,
+        userSubscriptions,
+        isLoading,
+        errorMessage,
+        entreprise,
+        googleBusiness,
+        logout,
+        handleResubscribe,
+        handleCancelSubscription,
+        verifyTokenWithServer,
+        refreshAccessToken,
+        handleError,
+        clearError,
+        fetchUserSubscriptions,
+        fetchUserDetails,
+      }}
     >
       {children}
     </AuthContext.Provider>
   );
 };
-
-export default AuthProvider;
