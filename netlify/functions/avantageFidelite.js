92a4c42a0c90b4c02c3daebef4d0a3ccb62c75cd
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-02-29 10:29:20 +0100

    Description des modifications apportées

--- netlify/functions/avantageFidelite.js
+++ netlify/functions/avantageFidelite.js
@@ -22,16 +22,17 @@ exports.handler = async (event) => {
       console.log('Traitement d\'une requête GET');
       const selectQuery = 'SELECT avantages_fidelite FROM users WHERE uuid = ?';
       console.log(`Exécution de la requête: ${selectQuery} avec uuid = ${userUuid}`);
-      const results = await conn.execute(selectQuery, [userUuid]);
-      console.log('Résultats de la requête:', results);
 
-      if (!Array.isArray(results) || results.length === 0 || !Array.isArray(results[0]) || results[0].length === 0) {
+      // Extraction des résultats de la requête
+      const [rows] = await conn.execute(selectQuery, [userUuid]);
+      console.log('Résultats de la requête:', rows);
+
+      if (rows.length === 0) {
         console.log('Utilisateur non trouvé ou aucun avantage pour le uuid:', userUuid);
         return { statusCode: 404, headers, body: JSON.stringify({ message: 'Utilisateur non trouvé ou aucun avantage disponible.' }) };
       }
 
-      const rows = results[0];
-      const avantages = rows[0]?.avantages_fidelite ? rows[0].avantages_fidelite.split('; ') : [];
+      const avantages = rows[0].avantages_fidelite.split('; ');
       console.log('Avantages récupérés:', avantages);
       return { statusCode: 200, headers, body: JSON.stringify({ avantages }) };
     } if (event.httpMethod === 'POST') {
@@ -39,14 +40,10 @@ exports.handler = async (event) => {
       const { avantages } = JSON.parse(event.body);
       const avantagesString = avantages.join('; ');
       const updateQuery = 'UPDATE users SET avantages_fidelite = ? WHERE uuid = ?';
-      try {
-        await conn.execute(updateQuery, [avantagesString, userUuid]);
-        console.log('Avantages mis à jour pour l\'utilisateur:', userUuid);
-        return { statusCode: 200, headers, body: JSON.stringify({ message: 'Avantages mis à jour avec succès.' }) };
-      } catch (updateError) {
-        console.error('Erreur lors de la mise à jour des avantages:', updateError);
-        return { statusCode: 500, headers, body: JSON.stringify({ message: `Erreur lors de la mise à jour des avantages: ${updateError.message}` }) };
-      }
+
+      await conn.execute(updateQuery, [avantagesString, userUuid]);
+      console.log('Avantages mis à jour pour l\'utilisateur:', userUuid);
+      return { statusCode: 200, headers, body: JSON.stringify({ message: 'Avantages mis à jour avec succès.' }) };
     }
   } catch (error) {
     console.error('Erreur lors du traitement:', error);
