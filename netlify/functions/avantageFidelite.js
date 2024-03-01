02f8f2e4e3a753b1bf57228ae16794a3e3ceb8d1
Romspopopoms <153521393+Romspopopoms@users.noreply.github.com>
2024-03-01 12:28:07 +0100

    Description des modifications apportées

--- netlify/functions/avantageFidelite.js
+++ netlify/functions/avantageFidelite.js
@@ -47,7 +47,7 @@ exports.handler = async (event) => {
         body: JSON.stringify({ avantages }),
       };
 
-      // Traitement des requêtes POST pour mettre à jour les avantages
+    // Traitement des requêtes POST pour mettre à jour les avantages
     } if (event.httpMethod === 'POST') {
       const body = JSON.parse(event.body);
       userUuid = body.userUuid; // Réassignation de userUuid sans re-déclaration
@@ -59,7 +59,16 @@ exports.handler = async (event) => {
           body: JSON.stringify({ message: 'Données manquantes ou format incorrect.' }),
         };
       }
-      // Le reste de la logique POST...
+
+      const avantagesString = body.avantages.join(';');
+      const updateQuery = 'UPDATE users SET avantages_fidelite = ? WHERE uuid = ?';
+      await conn.execute(updateQuery, [avantagesString, userUuid]);
+
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify({ message: 'Avantages mis à jour avec succès.' }),
+      };
     }
   } catch (error) {
     console.error('Erreur lors du traitement:', error);
